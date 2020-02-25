9211f0aed6da6d650c58fd2ea5b327b36e5f6bc3
AllyPC <hikido20@up.edu>
2020-02-24 19:32:26 -0800

    Create class for PigGameState

---
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -0,0 +1,11 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.GameFramework.infoMessage.GameState;
+
+public class PigGameState extends GameState {
+public int player0_id = 0;
+public int player1_id = 1;
+public int player0_score;
+public int player1_score;
+public int 
+}

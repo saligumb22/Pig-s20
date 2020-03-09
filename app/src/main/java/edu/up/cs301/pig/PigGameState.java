763ef5a9a7fd27363b3e7432aa8dcc56b6ab3053
AllyPC <hikido20@up.edu>
2020-03-09 12:57:11 -0700

    Checkpoint1

--- app/src/main/java/edu/up/cs301/pig/PigGameState.java
+++ app/src/main/java/edu/up/cs301/pig/PigGameState.java
@@ -3,72 +3,63 @@ package edu.up.cs301.pig;
 import edu.up.cs301.game.GameFramework.infoMessage.GameState;
 
 public class PigGameState extends GameState {
-    private int player0_id;
-    private int player1_id;
-    private int player0_score;
-    private int player1_score;
+    private int playerID;
+    private int player0;
+    private int player1;
     private int runTotal;
     private int dieValue;
 
-    public void gameInitialize() {
-        player0_id = 0;
-        player1_id = 1;
-        player0_score = 0;
-        player1_score = 0;
+    public PigGameState() {
+        playerID = 0;
+        player0 = 0;
+        player1 = 0;
         runTotal = 0;
-        dieValue = 0;
+        dieValue = 1;
     }
 
     public PigGameState(PigGameState state) {
-        player0_id = state.getPlayer0_id();
-        player1_id = state.getPlayer1_id();
-        player0_score = state.getPlayer0_score();
-        player1_score = state.getPlayer1_score();
-        runTotal = state.getRunTotal();
-        dieValue = state.getDieValue();
+        this.playerID = state.playerID;
+        this.player0 = state.player0;
+        this.player1 = state.player1;
+        this.runTotal = state.runTotal;
+        this.dieValue = state.dieValue;
     }
 
-    public int getPlayer0_id() {
-        return this.player0_id;
+    public int getPlayerID() {
+        return playerID;
     }
 
-    public void setPlayer0_id(int player0id) {
-        this.player0_id = player0id;
+    public void setPlayerID(int pID) {
+        this.playerID = pID;
     }
 
-    public int getPlayer1_id() {
-        return this.player1_id;
+    public int getPlayer0() {
+        return player0;
     }
 
-    public void setPlayer1_id(int player1id) {
-        this.player1_id = player1id;
+    public void setPlayer0(int p0) {
+        this.player0 = p0;
     }
 
-    public int getPlayer0_score() {
-        return this.player0_score;
+    public int getPlayer1() {
+        return player1;
     }
 
-    public void setPlayer0_score(int player0score) {
-        this.player0_score = player0score;
+    public void setPlayer1(int p1) {
+        this.player1 = p1;
     }
 
-    public int getPlayer1_score() {
-        return this.player1_score;
-    }
-    public void setPlayer1_score (int player1score){
-        this.player1_score = player1score;
-    }
     public int getRunTotal() {
-        return this.runTotal;
+        return runTotal;
     }
-    public void setRunTotalrunTotal (int updatedTotal){
-        this.runTotal = updatedTotal;
+    public void setRunTotalrunTotal (int rT){
+        this.runTotal = rT;
     }
     public int getDieValue() {
-        return this.dieValue;
+        return dieValue;
     }
-    public void setDieValue (int newDieValue){
-        this.runTotal = newDieValue;
+    public void setDieValue (int vD){
+        this.runTotal = vD;
     }
 
 
